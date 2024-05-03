bbf62682182a59946c1099f04393f21d9bcbf454
vest-exe <samkbrown91@icloud.com>
2024-05-03 23:17:02 +1000

    Start implementing parallel routs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Inter } from "next/font/google";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
+import React from "react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,8 +22,10 @@ export const metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
@@ -33,6 +36,8 @@ export default function RootLayout({
       <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
         <TopNav />
         {children}
+        {modal}
+        <div id="modal-root" />
       </body>
     </html>
     </ClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
@@ -11,12 +12,14 @@ async function Images() {
     <div className="flex flex-wrap justify-center gap-4 p-4">
       {images.map((image) => (
         <div key={image.id} className="flex flex-col h-48 w-48">
+          <Link href={`/img/${image.id}`}>
           <Image
             src={image.url}
             style={{objectFit: "contain"}}
             width={192}
             height={192} 
             alt={image.name}/>
+            </Link>
           <div>{image.name}</div>
         </div>
       ))}
